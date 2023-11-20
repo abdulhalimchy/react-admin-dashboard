e170e6fae296091bc6cd298036bb6423ce590b8e
Abdul Halim Chowdhury <salman@inverseai.com>
2023-11-21 01:16:10 +0600

    React Admin Dashboard

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import Home from "./pages/home/Home.tsx";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import Menu from "./components/menu/Menu.tsx";
+import Users from "./pages/users/Users.tsx";
+import Products from "./pages/products/Products.tsx";
+import Login from "./pages/login/Login.tsx";
+import "./styles/global.scss";
+
+const Layout = () => {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Admin Dashboard</span>
+      <span>Copyright@2023 React Admin Dashboard </span>
+    </div>
+  );
+};
+
+export default Footer;
